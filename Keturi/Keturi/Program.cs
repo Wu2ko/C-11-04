106bdd96a9db2d85e678e1dbd3d18558557607fb
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-11-04 13:50:20 +0200

    Add files via upload

---
+++ Keturi/Keturi/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Keturi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            if (float.TryParse(Console.ReadLine(), out float pirmas))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            if (float.TryParse(Console.ReadLine(), out float antras))
+            {
+             
+            }
+            else
+            {
+                Console.WriteLine("no2");
+            }
+
+            float trys = pirmas * antras;
+            Console.WriteLine(trys);
+
+
+
+
+
+            /* Console.WriteLine("Provide lenght: ");
+            while (isNumber == 0)
+            {
+                                
+                if (float.TryParse(Console.ReadLine(), out float leght))
+                {
+                    Console.WriteLine();
+                    int isNumber = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number");
+                    int isNumber = 0;
+                }
+            }
+
+
+            Console.WriteLine("Provide width");
+            if (float.TryParse(Console.ReadLine(), out float width))
+            {
+                Console.WriteLine();
+                int isNumber = 1;
+            }
+            else
+            {
+                Console.WriteLine("Not a number");
+                int isNumber = 0;
+            }
+
+
+
+            
+
+            float area = leght * width;
+            Console.WriteLine("Area is: ", area);
+
+            Console.WriteLine(area.ToString("0.00"));
+           
+*/
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
